6e0e1f2e950ec1fb5ebc886eb28a90d75dfa8b02
Sam <smd226@lehigh.edu>
2024-10-15 13:49:01 -0400

    added 2nd test

--- backend/src/test/MockDataRowTest.java
+++ backend/src/test/MockDataRowTest.java
@@ -29,4 +29,15 @@ public class MockDataRowTest extends TestCase{
         assertTrue(d.mId() == id);
         assertFalse(d.mCreated() == null); // this can help catch subtle bugs!
     }
+    public void testConstructor2(){
+        String message = "Test Message";
+        int likes = 200;
+        int id = 0;
+        MockDataRow d = new MockDataRow(id, message, likes, null); //date is set to null at the moment
+
+        assertTrue(d.mMessage().equals(message));
+        assertTrue(d.mLikes() == likes);
+        assertTrue(d.mId() == id);
+        assertFalse(d.mCreated() == null); // this can help catch subtle bugs!
+    }
 }
