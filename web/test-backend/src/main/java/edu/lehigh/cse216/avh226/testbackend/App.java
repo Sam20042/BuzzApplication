e957c28bf7f33298c23262cafddb2d86514532ee
Anders Heyniger <anders.heyniger@gmail.com>
2024-10-21 11:05:41 -0400

    created Maven package to create a mock backend for the sake of testing frontend

---
+++ web/test-backend/src/main/java/edu/lehigh/cse216/avh226/testbackend/App.java
@@ -0,0 +1,13 @@
+package edu.lehigh.cse216.avh226.testbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
