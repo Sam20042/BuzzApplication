8a6a5a6fde1f2c3ab4eb0a3aca0f95ed3070b0b5
Sam <smd226@lehigh.edu>
2024-11-22 19:27:20 -0500

    fixed rendering

--- mobile/flutter/ideas_application/lib/pages/login_page.dart
+++ mobile/flutter/ideas_application/lib/pages/login_page.dart
@@ -89,130 +89,132 @@ void wrongPasswordMessage(){
       backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 50),
-
-              // logo
-              const Icon(
-                Icons.lock,
-                size: 100,
-              ),
-              const SizedBox(height: 50),
-
-              // welcome back
-              Text(
-                'TheBuzz',
-                style: TextStyle(
-                  color: const Color.fromARGB(255, 42, 127, 197),
-                  fontSize: 20,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 50),
+            
+                // logo
+                const Icon(
+                  Icons.lock,
+                  size: 100,
                 ),
-              ),
-              const SizedBox(height: 25),
-
-              // email textfield
-              MyTextField(
-                controller: emailController,
-                hintText: 'Email',
-                obscureText: false,
-              ),
-
-              const SizedBox(height: 10),
-
-              // password textfield
-              MyTextField(
-                controller: passwordController,
-                hintText: 'Password',
-                obscureText: true,
-              ),
-
-              const SizedBox(height: 10),
-
-              // forgot password?
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                      'Forgot Password?',
-                      style: TextStyle(color: Colors.grey[600]),
-                    ),
-                  ],
-                ),
-              ),
-
-              const SizedBox(height: 25),
-
-              // sign in button
-              MyButton(
-                onTap: signUserIn,
-              ),
-
-              const SizedBox(height: 50),
-
-              // or continue with ...
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey[400],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Text(
-                        'Or continue with',
-                        style: TextStyle(color: Colors.grey[700]),
-                      ),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey[400],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              const SizedBox(height: 50),
-
-              // google sign in button
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.center,
-              //   children: const [
-              //     SquareTile(imagePath: 'lib/images/googlelogo.png'),
-              //     SizedBox(width: 25),
-              //   ],
-              // ),
-
-              ElevatedButton(
-                onPressed: () => AuthService().signInWithGoogle(), 
-                child: const Text("Google Sign In"),
-              ),
-
-              const SizedBox(height: 50),
-
-              // Don't have an account? Create one here
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("Don't have an account? "),
-                  Text(
-                    "Create one here",
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.bold,
-                    ),
+                const SizedBox(height: 50),
+            
+                // welcome back
+                Text(
+                  'TheBuzz',
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 42, 127, 197),
+                    fontSize: 20,
                   ),
-                ],
-              ),
-            ],
+                ),
+                const SizedBox(height: 25),
+            
+                // email textfield
+                MyTextField(
+                  controller: emailController,
+                  hintText: 'Email',
+                  obscureText: false,
+                ),
+            
+                const SizedBox(height: 10),
+            
+                // password textfield
+                MyTextField(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+            
+                const SizedBox(height: 10),
+            
+                // forgot password?
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Forgot Password?',
+                        style: TextStyle(color: Colors.grey[600]),
+                      ),
+                    ],
+                  ),
+                ),
+            
+                const SizedBox(height: 25),
+            
+                // sign in button
+                MyButton(
+                  onTap: signUserIn,
+                ),
+            
+                const SizedBox(height: 50),
+            
+                // or continue with ...
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          'Or continue with',
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+            
+                const SizedBox(height: 50),
+            
+                // google sign in button
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                //   children: const [
+                //     SquareTile(imagePath: 'lib/images/googlelogo.png'),
+                //     SizedBox(width: 25),
+                //   ],
+                // ),
+            
+                ElevatedButton(
+                  onPressed: () => AuthService().signInWithGoogle(), 
+                  child: const Text("Google Sign In"),
+                ),
+            
+                const SizedBox(height: 50),
+            
+                // Don't have an account? Create one here
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Don't have an account? "),
+                    Text(
+                      "Create one here",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
